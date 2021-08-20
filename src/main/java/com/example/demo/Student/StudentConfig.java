7523d410885cb9adbd2eab699bb6ba749c9ebe80
BEN SALAH Abderrahman <40049722+xDEVELYNNx@users.noreply.github.com>
2021-08-21 00:01:03 +0100

    file config for data integration in db

---
+++ src/main/java/com/example/demo/Student/StudentConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.Student;
+
+import org.apache.tomcat.jni.Local;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student sanaa = new Student(
+                    "BAHLOULI Sanaa",
+                    "Sanaabhl@gmail.com",
+                    LocalDate.of(1999,12,29),
+                    21
+            );
+        };
+    }
+}
